8451392327f934655c2475370c33131b479f5bd0
brian <brian.tan88@gmail.com>
2024-08-08 23:32:41 -0400

    xt6

---
+++ app/lucky/page.tsx
@@ -0,0 +1,269 @@
+import Head from "next/head";
+
+function Archive() {
+  return (
+    <>
+      <Head>
+        <title>Archive</title>
+      </Head>
+      <div>
+        <h1>List of Links</h1>
+        <ul>
+          <li>
+            <a href="https://flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco">
+              Changing My Mind on Remote About Being in San Francisco
+            </a>
+          </li>
+          <li>
+            <a href="https://blog.mattneary.com/worse-is-better">
+              Worse is Better
+            </a>
+          </li>
+          <li>
+            <a href="https://rfc-editor.org/rfc/rfc1178">RFC 1178</a>
+          </li>
+          <li>
+            <a href="https://inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html">
+              Tech Companies Keep Falling for the Forever Fallacy
+            </a>
+          </li>
+          <li>
+            <a href="https://slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html">
+              How to Cook Onions
+            </a>
+          </li>
+          <li>
+            <a href="https://careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and">
+              The Forever Fallacy
+            </a>
+          </li>
+          <li>
+            <a href="https://edconway.substack.com/p/watching-paint-dry">
+              Watching Paint Dry
+            </a>
+          </li>
+          <li>
+            <a href="https://engineersneedart.com/blog/samestop/samestop.html">
+              Samestop
+            </a>
+          </li>
+          <li>
+            <a href="https://write.as/synth-ruiner/the-low-tech-manifesto">
+              The Low Tech Manifesto
+            </a>
+          </li>
+          <li>
+            <a href="https://tinybird.info/reputation-laundering">
+              Reputation Laundering
+            </a>
+          </li>
+          <li>
+            <a href="https://subpixel.space/entries/life-after-lifestyle">
+              Life After Lifestyle
+            </a>
+          </li>
+          <li>
+            <a href="https://colin-scott.github.io/personal_website/research/interactive_latency.html">
+              Interactive Latency
+            </a>
+          </li>
+          <li>
+            <a href="https://archive.ph/VIbMD">Archived Page</a>
+          </li>
+          <li>
+            <a href="https://bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the">
+              Leveraging GPT-4 to Automate
+            </a>
+          </li>
+          <li>
+            <a href="https://reproof.app/blog/notes-apps-help-us-forget">
+              Notes Apps Help Us Forget
+            </a>
+          </li>
+          <li>
+            <a href="https://gaultier.github.io/blog/x11_x64.html">X11 x64</a>
+          </li>
+          <li>
+            <a href="https://ntietz.com/blog/ai-text-should-be-labeled">
+              AI Text Should Be Labeled
+            </a>
+          </li>
+          <li>
+            <a href="https://how-i-experience-web-today.com">
+              How I Experience Web Today
+            </a>
+          </li>
+          <li>
+            <a href="https://web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html">
+              Archived Blog on Cosine
+            </a>
+          </li>
+          <li>
+            <a href="https://wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105">
+              West Coast States Rode the Tech Boom
+            </a>
+          </li>
+          <li>
+            <a href="https://neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective">
+              Understanding Vectors from a Machine Learning Perspective
+            </a>
+          </li>
+          <li>
+            <a href="https://quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr">
+              Not So Quick PMR
+            </a>
+          </li>
+          <li>
+            <a href="https://freshpaint.io/blog/github-copilot-and-the-methods-of-rationality">
+              GitHub Copilot and the Methods of Rationality
+            </a>
+          </li>
+          <li>
+            <a href="https://iucompilercourse.github.io/tutorial-web-page">
+              Tutorial Web Page
+            </a>
+          </li>
+          <li>
+            <a href="https://jvns.ca/blog/2023/06/05/some-blogging-myths">
+              Some Blogging Myths
+            </a>
+          </li>
+          <li>
+            <a href="https://nskyc.com">NSKYC</a>
+          </li>
+          <li>
+            <a href="https://thezbook.com/code-first-vs-product-first">
+              Code First vs Product First
+            </a>
+          </li>
+          <li>
+            <a href="https://notes.jordanscales.com/block-the-blue">
+              Block the Blue
+            </a>
+          </li>
+          <li>
+            <a href="https://wy-lang.org">WY Lang</a>
+          </li>
+          <li>
+            <a href="https://mina-marketti.ocnk.net/product-list/206">
+              Mina Marketti
+            </a>
+          </li>
+          <li>
+            <a href="https://austinhenley.com/blog/makinggames.html">
+              Making Games
+            </a>
+          </li>
+          <li>
+            <a href="https://dwheeler.com/essays/filenames-in-shell.html">
+              Filenames in Shell
+            </a>
+          </li>
+          <li>
+            <a href="https://saurabhs.org/advanced-macos-commands">
+              Advanced MacOS Commands
+            </a>
+          </li>
+          <li>
+            <a href="https://neal.fun/password-game">Password Game</a>
+          </li>
+          <li>
+            <a href="https://mowglii.com/itsycal/datetime.html">
+              Itsycal DateTime
+            </a>
+          </li>
+          <li>
+            <a href="https://cooklang.org">Cooklang</a>
+          </li>
+          <li>
+            <a href="https://washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed">
+              Twitter X Links Delayed
+            </a>
+          </li>
+          <li>
+            <a href="https://wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794">
+              Ghost Job Listings
+            </a>
+          </li>
+          <li>
+            <a href="https://code.golf">Code Golf</a>
+          </li>
+          <li>
+            <a href="https://blog.robertelder.org/how-to-make-a-cpu">
+              How to Make a CPU
+            </a>
+          </li>
+          <li>
+            <a href="https://milan.cvitkovic.net/writing/things_youre_allowed_to_do">
+              Things You're Allowed to Do
+            </a>
+          </li>
+          <li>
+            <a href="https://jmmv.dev/2020/03/test-bracket.html">
+              Test Bracket
+            </a>
+          </li>
+          <li>
+            <a href="https://mindingourway.com/obvious-advice">
+              Obvious Advice
+            </a>
+          </li>
+          <li>
+            <a href="https://programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html">
+              What is Your Time Really Worth to You
+            </a>
+          </li>
+          <li>
+            <a href="https://johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail">
+              Reality Has a Surprising Amount of Detail
+            </a>
+          </li>
+          <li>
+            <a href="https://scattered-thoughts.net/writing/there-are-no-strings-on-me">
+              There Are No Strings on Me
+            </a>
+          </li>
+          <li>
+            <a href="https://danluu.com/why-benchmark">Why Benchmark</a>
+          </li>
+          <li>
+            <a href="https://quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220">
+              Biggest Discoveries in Computer Science in 2023
+            </a>
+          </li>
+          <li>
+            <a href="https://soggy.cat">Soggy Cat</a>
+          </li>
+          <li>
+            <a href="https://www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/">
+              The User is on Their Own
+            </a>
+          </li>
+          <li>
+            <a href="https://layoffs.fyi">Layoffs</a>
+          </li>
+          <li>
+            <a href="https://lastfun.site/intent">Intent</a>
+          </li>
+          <li>
+            <a href="https://gossipsweb.net/personal-websites">
+              Personal Websites
+            </a>
+          </li>
+          <li>
+            <a href="https://jimmyhmiller.github.io/ugliest-beautiful-codebase">
+              Ugliest Beautiful Codebase
+            </a>
+          </li>
+          <li>
+            <a href="https://thecut.com/2023/11/adulthood-is-a-mirage.html">
+              Adulthood is a Mirage
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Archive;
--- app/stuff/archivepage/page.tsx
+++ app/stuff/archivepage/page.tsx
@@ -9,85 +9,298 @@ export default function Archive() {
       </Head>
       <div>
         <p>
-          flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco
-        </p>
-        <p>blog.mattneary.com/worse-is-better</p>
-        <p>rfc-editor.org/rfc/rfc1178</p>
-        <p>
-          inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html
+          <a href="https://nskyc.com">[NSKYC](nskyc.com)</a>
         </p>
         <p>
-          slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html
+          <a href="https://wy-lang.org">[WY Lang](wy-lang.org)</a>
         </p>
         <p>
-          careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and
-        </p>
-        <p>edconway.substack.com/p/watching-paint-dry</p>
-        <p>engineersneedart.com/blog/samestop/samestop.html</p>
-        <p>write.as/synth-ruiner/the-low-tech-manifesto</p>
-        <p>tinybird.info/reputation-laundering</p>
-        <p>subpixel.space/entries/life-after-lifestyle</p>
-        <p>
-          colin-scott.github.io/personal_website/research/interactive_latency.html
-        </p>
-        <p>archive.ph/VIbMD</p>
-        <p>bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the</p>
-        <p>reproof.app/blog/notes-apps-help-us-forget</p>
-        <p>gaultier.github.io/blog/x11_x64.html</p>
-        <p>ntietz.com/blog/ai-text-should-be-labeled</p>
-        <p>how-i-experience-web-today.com</p>
-        <p>
-          web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html
+          <a href="https://code.golf">[Code Golf](code.golf)</a>
         </p>
         <p>
-          wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105
+          <a href="https://soggy.cat">[Soggy Cat](soggy.cat)</a>
         </p>
         <p>
-          neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective
-        </p>
-        <p>quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr</p>
-        <p>freshpaint.io/blog/github-copilot-and-the-methods-of-rationality</p>
-        <p>iucompilercourse.github.io/tutorial-web-page</p>
-        <p>jvns.ca/blog/2023/06/05/some-blogging-myths</p>
-        <p>nskyc.com</p>
-        <p>thezbook.com/code-first-vs-product-first</p>
-        <p>notes.jordanscales.com/block-the-blue</p>
-        <p>wy-lang.org</p>
-        <p>mina-marketti.ocnk.net/product-list/206</p>
-        <p>austinhenley.com/blog/makinggames.html</p>
-        <p>dwheeler.com/essays/filenames-in-shell.html</p>
-        <p>saurabhs.org/advanced-macos-commands</p>
-        <p>neal.fun/password-game</p>
-        <p>mowglii.com/itsycal/datetime.html</p>
-        <p>cooklang.org</p>
-        <p>washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed</p>
-        <p>
-          wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794
-        </p>
-        <p>motherfuckingwebsite.com</p>
-        <p>code.golf</p>
-        <p>blog.robertelder.org/how-to-make-a-cpu</p>
-        <p>milan.cvitkovic.net/writing/things_youre_allowed_to_do</p>
-        <p>jmmv.dev/2020/03/test-bracket.html</p>
-        <p>mindingourway.com/obvious-advice</p>
-        <p>
-          programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html
+          <a href="https://cooklang.org">[Cooklang](cooklang.org)</a>
         </p>
         <p>
-          johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail
+          <a href="https://layoffs.fyi">[Layoffs](layoffs.fyi)</a>
         </p>
-        <p>scattered-thoughts.net/writing/there-are-no-strings-on-me</p>
-        <p>danluu.com/why-benchmark</p>
         <p>
-          quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220
+          <a href="https://archive.ph/VIbMD">
+            [Archived Page](archive.ph/VIbMD)
+          </a>
         </p>
-        <p>soggy.cat</p>
-        <p>www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/</p>
-        <p>layoffs.fyi</p>
-        <p>lastfun.site/intent</p>
-        <p>gossipsweb.net/personal-websites</p>
-        <p>jimmyhmiller.github.io/ugliest-beautiful-codebase</p>
-        <p>thecut.com/2023/11/adulthood-is-a-mirage.html</p>
+        <p>
+          <a href="https://lastfun.site/intent">
+            [Intent](lastfun.site/intent)
+          </a>
+        </p>
+        <p>
+          <a href="https://danluu.com/why-benchmark">
+            [Why Benchmark](danluu.com/why-benchmark)
+          </a>
+        </p>
+        <p>
+          <a href="https://neal.fun/password-game">
+            [Password Game](neal.fun/password-game)
+          </a>
+        </p>
+        <p>
+          <a href="https://rfc-editor.org/rfc/rfc1178">
+            [RFC 1178](rfc-editor.org/rfc/rfc1178)
+          </a>
+        </p>
+        <p>
+          <a href="https://blog.mattneary.com/worse-is-better">
+            [Worse is Better](blog.mattneary.com/worse-is-better)
+          </a>
+        </p>
+        <p>
+          <a href="https://how-i-experience-web-today.com">
+            [How I Experience Web Today](how-i-experience-web-today.com)
+          </a>
+        </p>
+        <p>
+          <a href="https://gossipsweb.net/personal-websites">
+            [Personal Websites](gossipsweb.net/personal-websites)
+          </a>
+        </p>
+        <p>
+          <a href="https://jvns.ca/blog/2023/06/05/some-blogging-myths">
+            [Some Blogging Myths](jvns.ca/blog/2023/06/05/some-blogging-myths)
+          </a>
+        </p>
+        <p>
+          <a href="https://thecut.com/2023/11/adulthood-is-a-mirage.html">
+            [Adulthood is a
+            Mirage](thecut.com/2023/11/adulthood-is-a-mirage.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr">
+            [Not So Quick
+            PMR](quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr)
+          </a>
+        </p>
+        <p>
+          <a href="https://reproof.app/blog/notes-apps-help-us-forget">
+            [Notes Apps Help Us
+            Forget](reproof.app/blog/notes-apps-help-us-forget)
+          </a>
+        </p>
+        <p>
+          <a href="https://scattered-thoughts.net/writing/there-are-no-strings-on-me">
+            [There Are No Strings on
+            Me](scattered-thoughts.net/writing/there-are-no-strings-on-me)
+          </a>
+        </p>
+        <p>
+          <a href="https://freshpaint.io/blog/github-copilot-and-the-methods-of-rationality">
+            [GitHub Copilot and the Methods of
+            Rationality](freshpaint.io/blog/github-copilot-and-the-methods-of-rationality)
+          </a>
+        </p>
+        <p>
+          <a href="https://johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail">
+            [Reality Has a Surprising Amount of
+            Detail](johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail)
+          </a>
+        </p>
+        <p>
+          <a href="https://colin-scott.github.io/personal_website/research/interactive_latency.html">
+            [Interactive
+            Latency](colin-scott.github.io/personal_website/research/interactive_latency.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed">
+            [Twitter X Links
+            Delayed](washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed)
+          </a>
+        </p>
+        <p>
+          <a href="https://quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220">
+            [Biggest Discoveries in Computer Science in
+            2023](quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220)
+          </a>
+        </p>
+        <p>
+          <a href="https://neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective">
+            [Understanding Vectors from a Machine Learning
+            Perspective](neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective)
+          </a>
+        </p>
+        <p>
+          <a href="https://web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html">
+            [Archived Blog on
+            Cosine](web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and">
+            [The Forever
+            Fallacy](careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and)
+          </a>
+        </p>
+        <p>
+          <a href="https://wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105">
+            [West Coast States Rode the Tech
+            Boom](wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105)
+          </a>
+        </p>
+        <p>
+          <a href="https://slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html">
+            [How to Cook
+            Onions](slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html">
+            [Tech Companies Keep Falling for the Forever
+            Fallacy](inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco">
+            [Changing My Mind on Remote About Being in San
+            Francisco](flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco)
+          </a>
+        </p>
+        <p>
+          <a href="https://programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html">
+            [What is Your Time Really Worth to
+            You](programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/">
+            [The User is on Their
+            Own](www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/)
+          </a>
+        </p>
+        <p>
+          <a href="https://bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the">
+            [Leveraging GPT-4 to
+            Automate](bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the)
+          </a>
+        </p>
+        <p>
+          <a href="https://jimmyhmiller.github.io/ugliest-beautiful-codebase">
+            [Ugliest Beautiful
+            Codebase](jimmyhmiller.github.io/ugliest-beautiful-codebase)
+          </a>
+        </p>
+        <p>
+          <a href="https://milan.cvitkovic.net/writing/things_youre_allowed_to_do">
+            [Things You're Allowed to
+            Do](milan.cvitkovic.net/writing/things_youre_allowed_to_do)
+          </a>
+        </p>
+        <p>
+          <a href="https://engineersneedart.com/blog/samestop/samestop.html">
+            [Samestop](engineersneedart.com/blog/samestop/samestop.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://iucompilercourse.github.io/tutorial-web-page">
+            [Tutorial Web Page](iucompilercourse.github.io/tutorial-web-page)
+          </a>
+        </p>
+        <p>
+          <a href="https://write.as/synth-ruiner/the-low-tech-manifesto">
+            [The Low Tech
+            Manifesto](write.as/synth-ruiner/the-low-tech-manifesto)
+          </a>
+        </p>
+        <p>
+          <a href="https://subpixel.space/entries/life-after-lifestyle">
+            [Life After Lifestyle](subpixel.space/entries/life-after-lifestyle)
+          </a>
+        </p>
+        <p>
+          <a href="https://mina-marketti.ocnk.net/product-list/206">
+            [Mina Marketti](mina-marketti.ocnk.net/product-list/206)
+          </a>
+        </p>
+        <p>
+          <a href="https://thezbook.com/code-first-vs-product-first">
+            [Code First vs Product
+            First](thezbook.com/code-first-vs-product-first)
+          </a>
+        </p>
+        <p>
+          <a href="https://dwheeler.com/essays/filenames-in-shell.html">
+            [Filenames in Shell](dwheeler.com/essays/filenames-in-shell.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://ntietz.com/blog/ai-text-should-be-labeled">
+            [AI Text Should Be
+            Labeled](ntietz.com/blog/ai-text-should-be-labeled)
+          </a>
+        </p>
+        <p>
+          <a href="https://jmmv.dev/2020/03/test-bracket.html">
+            [Test Bracket](jmmv.dev/2020/03/test-bracket.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://mindingourway.com/obvious-advice">
+            [Obvious Advice](mindingourway.com/obvious-advice)
+          </a>
+        </p>
+        <p>
+          <a href="https://mowglii.com/itsycal/datetime.html">
+            [Itsycal DateTime](mowglii.com/itsycal/datetime.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://blog.robertelder.org/how-to-make-a-cpu">
+            [How to Make a CPU](blog.robertelder.org/how-to-make-a-cpu)
+          </a>
+        </p>
+        <p>
+          <a href="https://austinhenley.com/blog/makinggames.html">
+            [Making Games](austinhenley.com/blog/makinggames.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://gaultier.github.io/blog/x11_x64.html">
+            [X11 x64](gaultier.github.io/blog/x11_x64.html)
+          </a>
+        </p>
+        <p>
+          <a href="https://motherfuckingwebsite.com/">
+            [Motherfucking Website](motherfuckingwebsite.com)
+          </a>
+        </p>
+        <p>
+          <a href="https://edconway.substack.com/p/watching-paint-dry">
+            [Watching Paint Dry](edconway.substack.com/p/watching-paint-dry)
+          </a>
+        </p>
+        <p>
+          <a href="https://tinybird.info/reputation-laundering">
+            [Reputation Laundering](tinybird.info/reputation-laundering)
+          </a>
+        </p>
+        <p>
+          <a href="https://saurabhs.org/advanced-macos-commands">
+            [Advanced MacOS Commands](saurabhs.org/advanced-macos-commands)
+          </a>
+        </p>
+        <p>
+          <a href="https://notes.jordanscales.com/block-the-blue">
+            [Block the Blue](notes.jordanscales.com/block-the-blue)
+          </a>
+        </p>
+        <p>
+          <a href="https://wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794">
+            [Ghost Job
+            Listings](wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794)
+          </a>
+        </p>
       </div>
     </>
   );
