73ef1a72e78641a21d1342d55df3f5a117f01824
brian <brian.tan88@gmail.com>
2024-08-09 00:01:36 -0400

    tabs

--- app/stuff/archivepage/page.tsx
+++ app/stuff/archivepage/page.tsx
@@ -9,294 +9,417 @@ export default function Archive() {
       </Head>
       <div>
         <p>
-          <a href="https://nskyc.com">[NSKYC](nskyc.com)</a>
+          <a href="https://nskyc.com" target="_blank">
+            [NSKYC](nskyc.com)
+          </a>
         </p>
         <p>
-          <a href="https://wy-lang.org">[WY Lang](wy-lang.org)</a>
+          <a href="https://wy-lang.org" target="_blank">
+            [WY Lang](wy-lang.org)
+          </a>
         </p>
         <p>
-          <a href="https://code.golf">[Code Golf](code.golf)</a>
+          <a href="https://code.golf" target="_blank">
+            [Code Golf](code.golf)
+          </a>
         </p>
         <p>
-          <a href="https://soggy.cat">[Soggy Cat](soggy.cat)</a>
+          <a href="https://soggy.cat" target="_blank">
+            [Soggy Cat](soggy.cat)
+          </a>
         </p>
         <p>
-          <a href="https://cooklang.org">[Cooklang](cooklang.org)</a>
+          <a href="https://cooklang.org" target="_blank">
+            [Cooklang](cooklang.org)
+          </a>
         </p>
         <p>
-          <a href="https://layoffs.fyi">[Layoffs](layoffs.fyi)</a>
+          <a href="https://layoffs.fyi" target="_blank">
+            [Layoffs](layoffs.fyi)
+          </a>
         </p>
         <p>
-          <a href="https://archive.ph/VIbMD">
+          <a href="https://archive.ph/VIbMD" target="_blank">
             [Archived Page](archive.ph/VIbMD)
           </a>
         </p>
         <p>
-          <a href="https://lastfun.site/intent">
+          <a href="https://lastfun.site/intent" target="_blank">
             [Intent](lastfun.site/intent)
           </a>
         </p>
         <p>
-          <a href="https://danluu.com/why-benchmark">
+          <a href="https://danluu.com/why-benchmark" target="_blank">
             [Why Benchmark](danluu.com/why-benchmark)
           </a>
         </p>
         <p>
-          <a href="https://neal.fun/password-game">
+          <a href="https://neal.fun/password-game" target="_blank">
             [Password Game](neal.fun/password-game)
           </a>
         </p>
         <p>
-          <a href="https://rfc-editor.org/rfc/rfc1178">
+          <a href="https://rfc-editor.org/rfc/rfc1178" target="_blank">
             [RFC 1178](rfc-editor.org/rfc/rfc1178)
           </a>
         </p>
         <p>
-          <a href="https://blog.mattneary.com/worse-is-better">
+          <a href="https://blog.mattneary.com/worse-is-better" target="_blank">
             [Worse is Better](blog.mattneary.com/worse-is-better)
           </a>
         </p>
         <p>
-          <a href="https://how-i-experience-web-today.com">
+          <a href="https://how-i-experience-web-today.com" target="_blank">
             [How I Experience Web Today](how-i-experience-web-today.com)
           </a>
         </p>
         <p>
-          <a href="https://gossipsweb.net/personal-websites">
+          <a href="https://gossipsweb.net/personal-websites" target="_blank">
             [Personal Websites](gossipsweb.net/personal-websites)
           </a>
         </p>
         <p>
-          <a href="https://jvns.ca/blog/2023/06/05/some-blogging-myths">
+          <a
+            href="https://jvns.ca/blog/2023/06/05/some-blogging-myths"
+            target="_blank"
+          >
             [Some Blogging Myths](jvns.ca/blog/2023/06/05/some-blogging-myths)
           </a>
         </p>
         <p>
-          <a href="https://thecut.com/2023/11/adulthood-is-a-mirage.html">
+          <a
+            href="https://thecut.com/2023/11/adulthood-is-a-mirage.html"
+            target="_blank"
+          >
             [Adulthood is a
             Mirage](thecut.com/2023/11/adulthood-is-a-mirage.html)
           </a>
         </p>
         <p>
-          <a href="https://quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr">
+          <a
+            href="https://quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr"
+            target="_blank"
+          >
             [Not So Quick
             PMR](quuxplusone.github.io/blog/2023/06/02/not-so-quick-pmr)
           </a>
         </p>
         <p>
-          <a href="https://reproof.app/blog/notes-apps-help-us-forget">
+          <a
+            href="https://reproof.app/blog/notes-apps-help-us-forget"
+            target="_blank"
+          >
             [Notes Apps Help Us
             Forget](reproof.app/blog/notes-apps-help-us-forget)
           </a>
         </p>
         <p>
-          <a href="https://scattered-thoughts.net/writing/there-are-no-strings-on-me">
+          <a
+            href="https://scattered-thoughts.net/writing/there-are-no-strings-on-me"
+            target="_blank"
+          >
             [There Are No Strings on
             Me](scattered-thoughts.net/writing/there-are-no-strings-on-me)
           </a>
         </p>
         <p>
-          <a href="https://freshpaint.io/blog/github-copilot-and-the-methods-of-rationality">
+          <a
+            href="https://freshpaint.io/blog/github-copilot-and-the-methods-of-rationality"
+            target="_blank"
+          >
             [GitHub Copilot and the Methods of
             Rationality](freshpaint.io/blog/github-copilot-and-the-methods-of-rationality)
           </a>
         </p>
         <p>
-          <a href="https://johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail">
+          <a
+            href="https://johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail"
+            target="_blank"
+          >
             [Reality Has a Surprising Amount of
             Detail](johnsalvatier.org/blog/2017/reality-has-a-surprising-amount-of-detail)
           </a>
         </p>
         <p>
-          <a href="https://colin-scott.github.io/personal_website/research/interactive_latency.html">
+          <a
+            href="https://colin-scott.github.io/personal_website/research/interactive_latency.html"
+            target="_blank"
+          >
             [Interactive
             Latency](colin-scott.github.io/personal_website/research/interactive_latency.html)
           </a>
         </p>
         <p>
-          <a href="https://washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed">
+          <a
+            href="https://washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed"
+            target="_blank"
+          >
             [Twitter X Links
             Delayed](washingtonpost.com/technology/2023/08/15/twitter-x-links-delayed)
           </a>
         </p>
         <p>
-          <a href="https://quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220">
+          <a
+            href="https://quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220"
+            target="_blank"
+          >
             [Biggest Discoveries in Computer Science in
             2023](quantamagazine.org/the-biggest-discoveries-in-computer-science-in-2023-20231220)
           </a>
         </p>
         <p>
-          <a href="https://neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective">
+          <a
+            href="https://neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective"
+            target="_blank"
+          >
             [Understanding Vectors from a Machine Learning
             Perspective](neptune.ai/blog/understanding-vectors-from-a-machine-learning-perspective)
           </a>
         </p>
         <p>
-          <a href="https://web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html">
+          <a
+            href="https://web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html"
+            target="_blank"
+          >
             [Archived Blog on
             Cosine](web.archive.org/web/20210513043002/web.eecs.utk.edu/~azh/blog/cosine.html)
           </a>
         </p>
         <p>
-          <a href="https://careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and">
+          <a
+            href="https://careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and"
+            target="_blank"
+          >
             [The Forever
             Fallacy](careershifters.org/expert-advice/the-forever-fallacy-or-why-youre-probably-not-going-to-find-your-perfect-career-and)
           </a>
         </p>
         <p>
-          <a href="https://wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105">
+          <a
+            href="https://wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105"
+            target="_blank"
+          >
             [West Coast States Rode the Tech
             Boom](wsj.com/articles/west-coast-states-rode-the-tech-boom-now-they-face-higher-unemployment-falling-wages-83318105)
           </a>
         </p>
         <p>
-          <a href="https://slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html">
+          <a
+            href="https://slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html"
+            target="_blank"
+          >
             [How to Cook
             Onions](slate.com/human-interest/2012/05/how-to-cook-onions-why-recipe-writers-lie-and-lie-about-how-long-they-take-to-caramelize.html)
           </a>
         </p>
         <p>
-          <a href="https://inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html">
+          <a
+            href="https://inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html"
+            target="_blank"
+          >
             [Tech Companies Keep Falling for the Forever
             Fallacy](inc.com/jason-aten/tech-companies-keep-falling-for-forever-fallacy-why-smart-leaders-should-know-better.html)
           </a>
         </p>
         <p>
-          <a href="https://flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco">
+          <a
+            href="https://flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco"
+            target="_blank"
+          >
             [Changing My Mind on Remote About Being in San
             Francisco](flocrivello.com/changing-my-mind-on-remote-about-being-in-san-francisco)
           </a>
         </p>
         <p>
-          <a href="https://programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html">
+          <a
+            href="https://programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html"
+            target="_blank"
+          >
             [What is Your Time Really Worth to
             You](programs.clearerthinking.org/what_is_your_time_really_worth_to_you.html)
           </a>
         </p>
         <p>
-          <a href="https://www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/">
+          <a
+            href="https://www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/"
+            target="_blank"
+          >
             [The User is on Their
             Own](www.selfawaresoup.com/notes/2024/05/01/the-user-is-on-their-own/)
           </a>
         </p>
         <p>
-          <a href="https://bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the">
+          <a
+            href="https://bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the"
+            target="_blank"
+          >
             [Leveraging GPT-4 to
             Automate](bit.kevinslin.com/p/leveraging-gpt-4-to-automate-the)
           </a>
         </p>
         <p>
-          <a href="https://jimmyhmiller.github.io/ugliest-beautiful-codebase">
+          <a
+            href="https://jimmyhmiller.github.io/ugliest-beautiful-codebase"
+            target="_blank"
+          >
             [Ugliest Beautiful
             Codebase](jimmyhmiller.github.io/ugliest-beautiful-codebase)
           </a>
         </p>
         <p>
-          <a href="https://milan.cvitkovic.net/writing/things_youre_allowed_to_do">
+          <a
+            href="https://milan.cvitkovic.net/writing/things_youre_allowed_to_do"
+            target="_blank"
+          >
             [Things You&apos;re Allowed to
             Do](milan.cvitkovic.net/writing/things_youre_allowed_to_do)
           </a>
         </p>
         <p>
-          <a href="https://engineersneedart.com/blog/samestop/samestop.html">
+          <a
+            href="https://engineersneedart.com/blog/samestop/samestop.html"
+            target="_blank"
+          >
             [Samestop](engineersneedart.com/blog/samestop/samestop.html)
           </a>
         </p>
         <p>
-          <a href="https://iucompilercourse.github.io/tutorial-web-page">
+          <a
+            href="https://iucompilercourse.github.io/tutorial-web-page"
+            target="_blank"
+          >
             [Tutorial Web Page](iucompilercourse.github.io/tutorial-web-page)
           </a>
         </p>
         <p>
-          <a href="https://write.as/synth-ruiner/the-low-tech-manifesto">
+          <a
+            href="https://write.as/synth-ruiner/the-low-tech-manifesto"
+            target="_blank"
+          >
             [The Low Tech
             Manifesto](write.as/synth-ruiner/the-low-tech-manifesto)
           </a>
         </p>
         <p>
-          <a href="https://subpixel.space/entries/life-after-lifestyle">
+          <a
+            href="https://subpixel.space/entries/life-after-lifestyle"
+            target="_blank"
+          >
             [Life After Lifestyle](subpixel.space/entries/life-after-lifestyle)
           </a>
         </p>
         <p>
-          <a href="https://mina-marketti.ocnk.net/product-list/206">
+          <a
+            href="https://mina-marketti.ocnk.net/product-list/206"
+            target="_blank"
+          >
             [Mina Marketti](mina-marketti.ocnk.net/product-list/206)
           </a>
         </p>
         <p>
-          <a href="https://thezbook.com/code-first-vs-product-first">
+          <a
+            href="https://thezbook.com/code-first-vs-product-first"
+            target="_blank"
+          >
             [Code First vs Product
             First](thezbook.com/code-first-vs-product-first)
           </a>
         </p>
         <p>
-          <a href="https://dwheeler.com/essays/filenames-in-shell.html">
+          <a
+            href="https://dwheeler.com/essays/filenames-in-shell.html"
+            target="_blank"
+          >
             [Filenames in Shell](dwheeler.com/essays/filenames-in-shell.html)
           </a>
         </p>
         <p>
-          <a href="https://ntietz.com/blog/ai-text-should-be-labeled">
+          <a
+            href="https://ntietz.com/blog/ai-text-should-be-labeled"
+            target="_blank"
+          >
             [AI Text Should Be
             Labeled](ntietz.com/blog/ai-text-should-be-labeled)
           </a>
         </p>
         <p>
-          <a href="https://jmmv.dev/2020/03/test-bracket.html">
+          <a href="https://jmmv.dev/2020/03/test-bracket.html" target="_blank">
             [Test Bracket](jmmv.dev/2020/03/test-bracket.html)
           </a>
         </p>
         <p>
-          <a href="https://mindingourway.com/obvious-advice">
+          <a href="https://mindingourway.com/obvious-advice" target="_blank">
             [Obvious Advice](mindingourway.com/obvious-advice)
           </a>
         </p>
         <p>
-          <a href="https://mowglii.com/itsycal/datetime.html">
+          <a href="https://mowglii.com/itsycal/datetime.html" target="_blank">
             [Itsycal DateTime](mowglii.com/itsycal/datetime.html)
           </a>
         </p>
         <p>
-          <a href="https://blog.robertelder.org/how-to-make-a-cpu">
+          <a
+            href="https://blog.robertelder.org/how-to-make-a-cpu"
+            target="_blank"
+          >
             [How to Make a CPU](blog.robertelder.org/how-to-make-a-cpu)
           </a>
         </p>
         <p>
-          <a href="https://austinhenley.com/blog/makinggames.html">
+          <a
+            href="https://austinhenley.com/blog/makinggames.html"
+            target="_blank"
+          >
             [Making Games](austinhenley.com/blog/makinggames.html)
           </a>
         </p>
         <p>
-          <a href="https://gaultier.github.io/blog/x11_x64.html">
+          <a
+            href="https://gaultier.github.io/blog/x11_x64.html"
+            target="_blank"
+          >
             [X11 x64](gaultier.github.io/blog/x11_x64.html)
           </a>
         </p>
         <p>
-          <a href="https://motherfuckingwebsite.com/">
+          <a href="https://motherfuckingwebsite.com/" target="_blank">
             [Motherfucking Website](motherfuckingwebsite.com)
           </a>
         </p>
         <p>
-          <a href="https://edconway.substack.com/p/watching-paint-dry">
+          <a
+            href="https://edconway.substack.com/p/watching-paint-dry"
+            target="_blank"
+          >
             [Watching Paint Dry](edconway.substack.com/p/watching-paint-dry)
           </a>
         </p>
         <p>
-          <a href="https://tinybird.info/reputation-laundering">
+          <a href="https://tinybird.info/reputation-laundering" target="_blank">
             [Reputation Laundering](tinybird.info/reputation-laundering)
           </a>
         </p>
         <p>
-          <a href="https://saurabhs.org/advanced-macos-commands">
+          <a
+            href="https://saurabhs.org/advanced-macos-commands"
+            target="_blank"
+          >
             [Advanced MacOS Commands](saurabhs.org/advanced-macos-commands)
           </a>
         </p>
         <p>
-          <a href="https://notes.jordanscales.com/block-the-blue">
+          <a
+            href="https://notes.jordanscales.com/block-the-blue"
+            target="_blank"
+          >
             [Block the Blue](notes.jordanscales.com/block-the-blue)
           </a>
         </p>
         <p>
-          <a href="https://wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794">
+          <a
+            href="https://wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794"
+            target="_blank"
+          >
             [Ghost Job
             Listings](wsj.com/articles/that-plum-job-listing-may-just-be-a-ghost-3aafc794)
           </a>
