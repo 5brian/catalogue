76650d800bba5b1cf7163188507ab757ed164d68
5brian <brian.tan88@gmail.com>
2024-12-11 07:20:04 -0500

    refactor: move links out of component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "brian",
-  description: "hi",
+  description: "hi this is me on the web",
 };
 
 export default function RootLayout({
