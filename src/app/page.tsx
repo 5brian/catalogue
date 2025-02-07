0abb898496659e8bc18de182fabd9899c887cdb3
5brian <brian.tan88@gmail.com>
2025-02-07 02:10:58 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Link href="../me">me</Link>
       </Module>
       <Module>
-        <Link href="https://5s55jrv5h.sites.cv/">work</Link>
+        <Link href="https://cwvk5jndg.sites.cv/">work</Link>
       </Module>
       <Module>
         <Link href="../uses">uses</Link>
