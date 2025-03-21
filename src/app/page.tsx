6f11e47fe41dea5bb5ca903aec38255c406e8e6e
brian tan <brian.tan88@gmail.com>
2025-03-21 15:20:30 -0400

    second light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
-import "@/styles/style.css";
 import React from "react";
-import Layout from "@/components/layout";
-import Module from "@/components/module";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <Layout title="brian tan">
-      <Module>
-        <Link href="../me">me</Link>
-      </Module>
-      <Module>
-        <Link href="../work">work</Link>
-      </Module>
-      <Module>
-        <Link href="../uses">uses</Link>
-      </Module>
-      <Module>
-        <Link href="../lucky" target="_blank">
-          i&apos;m feeling lucky
-        </Link>
-      </Module>
-    </Layout>
+    <div style={{ 
+      fontFamily: 'Monaco, monospace', 
+      color: 'white',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      paddingTop: '20vh',
+      paddingLeft: '20%'
+    }}>
+      <h1 style={{ marginBottom: '20px' }}>brian tan</h1>
+      <p style={{ margin: '5px 0' }}>b. 2004, santa rosa. undergrad math & computer science, incoming atc at the faa</p>
+      <p style={{ margin: '5px 0' }}>
+        <a href="mailto:hi@brian-tan.me" style={{ color: 'white', textDecoration: 'underline' }}>
+          hi@brian-tan.me
+        </a>
+      </p>
+      <p style={{ margin: '5px 0' }}>
+        <a href="https://github.com/5brian" style={{ color: 'white', textDecoration: 'underline' }}>
+          github.com/5brian
+        </a>
+      </p>
+    </div>
   );
 }
